bc642a7276b0c29e17a64821ad0fb8044338460c
wangtr <2880984060@qq.com>
2017-08-24 15:28:46 +0800

    新增编译文件

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+from wangtr/php_swoole
+ADD ./php.tar /root/php
+workdir /root/php
+CMD ["/bin/bash"]
---
+++ run-build.sh
@@ -0,0 +1,11 @@
+rm -rf build
+mkdir build
+git checkout master
+git archive -o ./build/php.tar master
+#mkdir build/php
+#tar xf ./build/php.tar -C ./build/php
+cp Dockerfile ./build/
+cd build
+docker build --no-cache=false -f Dockerfile -t wangtr/im_realtime
+cd ..
+rm -rf build
